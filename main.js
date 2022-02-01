fa7bac7a5172facd12aa441663a713773225ccc3
Satya prakash <satyaprakash934@gmail.com>
2022-02-02 00:27:59 +0530

    local storage as objects and loading data to frontend

--- main.js
+++ main.js
@@ -11,9 +11,14 @@ function store(e) {
   var name = document.getElementById('name').value
   var email = document.getElementById('email').value
 
-  localStorage.setItem('personname', name)
-  localStorage.setItem('personemail', email)
+  var person1 = {
+    name: name,
+    email: email,
+  }
 
+  localStorage.setItem('person1', JSON.stringify(person1))
+
+  console.log(person1)
   console.log(name)
   console.log(email)
 }
@@ -21,3 +26,11 @@ function store(e) {
 // ul.children[0].innerHTML = '<h4 style="color:green">DEMO</h4>'
 
 // ul.children[1].innerHTML = '<h4 style="color:yellow" >Satya</h4>'
+
+window.onload = function () {
+  // alert('loaded')
+  var user = JSON.parse(localStorage.getItem('person1'))
+  document.getElementById('pname').innerHTML = user.name
+  document.getElementById('pemail').innerHTML = user.email
+  console.log(user)
+}
