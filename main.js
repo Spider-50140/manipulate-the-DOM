c7c00b06ca500f4f7d8c021914b5b344b5f59882
Satya prakash <satyaprakash934@gmail.com>
2022-02-01 23:46:26 +0530

    Made search functionality

--- main.js
+++ main.js
@@ -39,13 +39,25 @@ function addItem(e) {
   console.log('entered')
 
   var newItem = document.getElementById('item').value
+  var newdes = document.getElementById('des').value
+
+  // console.log(newItem)
+  // console.log(newdes)
+
   var li = document.createElement('li')
   li.className = 'list-group-item'
   li.appendChild(document.createTextNode(newItem))
+
   var deleteBtn = document.createElement('button')
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
   deleteBtn.appendChild(document.createTextNode('X'))
+
+  var description = document.createElement('h6')
+  description.className = 'description'
+  description.appendChild(document.createTextNode(newdes))
+
   li.appendChild(deleteBtn)
+  li.appendChild(description)
   itemList.appendChild(li)
 }
 
@@ -58,4 +70,62 @@ function removeItem(e) {
       itemList.removeChild(li)
     }
   }
+}
+
+// filtering part
+
+var filter = document.getElementById('filter')
+filter.addEventListener('keyup', filterItems)
+
+function filterItems(e) {
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase()
+  // Get lis
+  var items = itemList.getElementsByTagName('li')
+  var items2 = itemList.getElementsByTagName('li')
+  // var desc = itemlist.getElementsByClassName('description')
+
+  // console.log(desc)
+
+  console.log(items)
+  // Convert to an array
+
+  Array.from(items).forEach(function (item) {
+    // console.log(item)
+    var itemName = item.firstChild.wholeText
+    // console.log(itemName)
+    var x = itemName.toLowerCase().indexOf(text)
+    console.log(x)
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block'
+    } else {
+      item.style.display = 'none'
+    }
+  })
+
+  // Array.from(desc).forEach(function (i) {
+  //   // console.log(item)
+  //   var descame = i.firstChild.wholeText
+  //   // console.log(itemName)
+  //   var x = descame.toLowerCase().indexOf(text)
+  //   console.log(x)
+  //   if (x != -1) {
+  //     i.style.display = 'block'
+  //   } else {
+  //     i.style.display = 'none'
+  //   }
+  // })
+
+  Array.from(items2).forEach(function (i) {
+    // console.log(item)
+    var descame = i.firstElementChild.innerText
+    // console.log(itemName)
+    var x = descame.toLowerCase().indexOf(text)
+    console.log(x)
+    if (x != -1) {
+      i.style.display = 'block'
+    } else {
+      i.style.display = 'none'
+    }
+  })
 }
